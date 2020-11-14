25e8159fa478a9c418e009ff0497d5ec3b757569
jesse harlan <gotsanity@gmail.com>
2020-11-14 13:52:57 -0800

    Add project files.

---
+++ Tilr.Tray/TilrMainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Tilr.Tray
+{
+    partial class TilrMainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.trayContextOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayContextSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.trayContextExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.trayContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.ContextMenuStrip = this.trayContextMenu;
+            this.trayIcon.Text = "Tilr";
+            this.trayIcon.Visible = true;
+            this.trayIcon.Click += new System.EventHandler(this.trayIcon_Click);
+            this.trayIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseClick);
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseDoubleClick);
+            // 
+            // trayContextMenu
+            // 
+            this.trayContextMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.trayContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trayContextOpen,
+            this.trayContextSettings,
+            this.toolStripSeparator2,
+            this.trayContextExit});
+            this.trayContextMenu.Name = "trayContextMenu";
+            this.trayContextMenu.Size = new System.Drawing.Size(157, 106);
+            this.trayContextMenu.Text = "test";
+            this.trayContextMenu.Opening += new System.ComponentModel.CancelEventHandler(this.trayContextMenu_Opening);
+            // 
+            // trayContextOpen
+            // 
+            this.trayContextOpen.Name = "trayContextOpen";
+            this.trayContextOpen.Size = new System.Drawing.Size(156, 32);
+            this.trayContextOpen.Text = "Open Tilr";
+            // 
+            // trayContextSettings
+            // 
+            this.trayContextSettings.Enabled = false;
+            this.trayContextSettings.Name = "trayContextSettings";
+            this.trayContextSettings.Size = new System.Drawing.Size(156, 32);
+            this.trayContextSettings.Text = "Settings";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(153, 6);
+            // 
+            // trayContextExit
+            // 
+            this.trayContextExit.Name = "trayContextExit";
+            this.trayContextExit.Size = new System.Drawing.Size(156, 32);
+            this.trayContextExit.Text = "Exit Tilr";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(197, 6);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(200, 32);
+            this.toolStripMenuItem1.Text = "Do Something";
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(800, 720);
+            this.tableLayoutPanel.TabIndex = 1;
+            // 
+            // TilrMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 721);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "TilrMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tilr";
+            this.Load += new System.EventHandler(this.TilrMainForm_Load);
+            this.SizeChanged += new System.EventHandler(this.TilrMainForm_SizeChanged);
+            this.trayContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip trayContextMenu;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem trayContextExit;
+        private System.Windows.Forms.ToolStripMenuItem trayContextOpen;
+        private System.Windows.Forms.ToolStripMenuItem trayContextSettings;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+    }
+}
+
---
+++ Tilr.Tray/TilrMainForm.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tilr.Tray
+{
+    public partial class TilrMainForm : Form
+    {
+        private int _gridSizeX = 10;
+        private int _gridSizeY = 10;
+        private bool _isSelectingGrid = false;
+        private int _selectionMinX, _selectionMinY, _selectionMaxX, _selectionMaxY;
+        private List<Button> cells = new List<Button>();
+        private IntPtr targetWindow;
+
+        public TilrMainForm()
+        {
+            InitializeComponent();
+            trayContextOpen.Click += new System.EventHandler(this.trayContextOpen_Click);
+            trayContextExit.Click += new System.EventHandler(this.trayContextExit_Click);
+
+            tableLayoutPanel.RowCount = _gridSizeY;
+            tableLayoutPanel.ColumnCount = _gridSizeX;
+
+            tableLayoutPanel.ColumnStyles.Clear();
+            tableLayoutPanel.RowStyles.Clear();
+
+            for (int i = 0; i < _gridSizeX; i++)
+            {
+                tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100 / _gridSizeX));
+            }
+            for (int i = 0; i < _gridSizeY; i++)
+            {
+                this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100 / _gridSizeY));
+            }
+
+            for (int y = 0; y < tableLayoutPanel.RowCount; y++)
+            {
+                for (int x = 0; x < tableLayoutPanel.ColumnCount; x++)
+                {
+                    var b = new Button();
+                    //b.Text = string.Format("({0},{1})", x, y);
+                    b.Name = string.Format("btnGridCell_{0}_{1}", x, y);
+                    b.Dock = DockStyle.Fill;
+                    b.Click += btnGridCell_Click;
+                    b.BackColor = Color.LightGray;
+                    b.MouseEnter += btnGridCell_MouseEnter;
+                    tableLayoutPanel.Controls.Add(b, x, y);
+                    
+                    cells.Add(b);
+                }
+            }
+        }
+
+        private void btnGridCell_Click(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+
+            if (b == null)
+            {
+                return;
+            }
+
+            if (!_isSelectingGrid)
+            {
+                this._isSelectingGrid = true;
+                _selectionMinX = tableLayoutPanel.GetColumn(b);
+                _selectionMinY = tableLayoutPanel.GetRow(b);
+                b.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                _isSelectingGrid = false;
+                _selectionMaxX = tableLayoutPanel.GetColumn(b);
+                _selectionMaxY = tableLayoutPanel.GetRow(b);
+                SetWindowToGridSelection();
+            }
+        }
+
+        private void btnGridCell_MouseEnter(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            
+            if (b != null && _isSelectingGrid)
+            {
+                int x = tableLayoutPanel.GetColumn(b);
+                int y = tableLayoutPanel.GetRow(b);
+
+                int minX, minY, maxX, maxY;
+
+                if (x <= _selectionMinX)
+                {
+                    minX = x;
+                    maxX = _selectionMinX;
+                } else
+                {
+                    maxX = x;
+                    minX = _selectionMinX;
+                }
+
+                if (y <= _selectionMinY)
+                {
+                    minY = y;
+                    maxY = _selectionMinY;
+                }
+                else
+                {
+                    maxY = y;
+                    minY = _selectionMinY;
+                }
+
+                foreach (Button cell in cells)
+                {
+                    if (!isInRange(tableLayoutPanel.GetColumn(cell), minX, maxX) || !isInRange(tableLayoutPanel.GetRow(cell), minY, maxY))
+                    {
+                        cell.BackColor = Color.LightGray;
+                    }
+                    else
+                    {
+                        cell.BackColor = Color.LightBlue;
+                    }
+                }
+
+                
+            }
+        }
+
+        private bool isInRange(int test, int min, int max)
+        {
+            return test >= min && test <= max;
+        }
+
+        private void SetWindowToGridSelection()
+        {
+            Debug.WriteLine(string.Format("Positioning window to coordinates {0},{1} to {2},{3}", _selectionMinX, _selectionMinY, _selectionMaxX, _selectionMaxY));
+            SetWindowSelection("Untitled - Notepad");
+
+            Debug.WriteLine(string.Format("screen bounds {0}", Screen.FromControl(this).Bounds));
+
+            Rectangle screenBounds = Screen.FromControl(this).Bounds;
+
+            int cellSizeX = (screenBounds.Width / _gridSizeX);
+            int cellSizeY = (screenBounds.Height / _gridSizeY);
+
+            int posX = cellSizeX * _selectionMinX;
+            int posY = cellSizeY * _selectionMinY;
+
+            int posMaxX = cellSizeX * (_selectionMaxX + 1);
+            int posMaxY = cellSizeY * (_selectionMaxY + 1);
+
+            int width = posMaxX - posX;
+            int height = posMaxY - posY;
+
+            ResizeWindow(width, height, posX, posY);
+        }
+
+        private void TilrMainForm_SizeChanged(object sender, EventArgs e)
+        {
+            bool MouseOnTaskbar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+
+            if (this.WindowState == FormWindowState.Minimized && MouseOnTaskbar)
+            {
+                trayIcon.Icon = SystemIcons.Application;
+                trayIcon.BalloonTipText = "Tilr is running in the system tray.";
+                trayIcon.ShowBalloonTip(1000);
+                this.ShowInTaskbar = false;
+                trayIcon.Visible = true;
+                trayContextMenu.Enabled = true;
+            }
+        }
+
+        private void TilrMainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            openTilrForm();
+        }
+
+        private void openTilrForm()
+        {
+            this.WindowState = FormWindowState.Normal;
+
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.ShowInTaskbar = true;
+                trayIcon.Visible = false;
+                trayContextMenu.Hide();
+            }
+        }
+
+        private void trayContextMenu_Opening(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void trayContextOpen_Click(object sender, EventArgs e)
+        {
+            openTilrForm();
+        }
+
+        private void trayContextExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void trayIcon_Click(object sender, EventArgs e)
+        {
+            trayContextMenu.Show(Cursor.Position);
+        }
+
+        private void trayIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            trayContextMenu.Show(Cursor.Position);
+        }
+
+        private void SetWindowSelection(string caption)
+        {
+            targetWindow = FindWindowByCaption(IntPtr.Zero, caption);
+            
+            if (targetWindow == IntPtr.Zero)
+            {
+                MessageBox.Show(
+                    "Could not find a window with the title \"" +
+                    caption + "\"");
+                return;
+            }
+        }
+
+        private void ResizeWindow(int width, int height, int posX, int posY)
+        {
+            Debug.WriteLine(string.Format("Setting window position to {0},{1} with a width/height of {2}/{3}", posX, posY, width, height));
+            SetWindowPos(targetWindow, IntPtr.Zero,
+                posX, posY, width, height, 0);
+        }
+
+        // ****************************************************************************
+        // Window resize functionality
+        // ****************************************************************************
+
+
+        // Define the FindWindow API function.
+        [DllImport("user32.dll", EntryPoint = "FindWindow",
+            SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly,
+            string lpWindowName);
+
+        // Define the SetWindowPos API function.
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPos(IntPtr hWnd,
+            IntPtr hWndInsertAfter, int X, int Y, int cx, int cy,
+            SetWindowPosFlags uFlags);
+
+        // Define the SetWindowPosFlags enumeration.
+        [Flags()]
+        private enum SetWindowPosFlags : uint
+        {
+            SynchronousWindowPosition = 0x4000,
+            DeferErase = 0x2000,
+            DrawFrame = 0x0020,
+            FrameChanged = 0x0020,
+            HideWindow = 0x0080,
+            DoNotActivate = 0x0010,
+            DoNotCopyBits = 0x0100,
+            IgnoreMove = 0x0002,
+            DoNotChangeOwnerZOrder = 0x0200,
+            DoNotRedraw = 0x0008,
+            DoNotReposition = 0x0200,
+            DoNotSendChangingEvent = 0x0400,
+            IgnoreResize = 0x0001,
+            IgnoreZOrder = 0x0004,
+            ShowWindow = 0x0040,
+        }
+    }
+}
